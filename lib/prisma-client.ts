3307498c553803c70fe434d525ea221abd4c4d0c
ihuvtech <ihuvtech@gmail.com>
2026-03-01 06:11:30 -0500

    Refactor Prisma client initialization with factory function

--- lib/prisma-client.ts
+++ lib/prisma-client.ts
@@ -2,11 +2,14 @@ import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
+function createPrismaClient() {
+  return new PrismaClient().$extends(withAccelerate());
+}
+
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient().$extends(withAccelerate());
+  globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
